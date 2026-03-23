fad9708cbc941e8025bc731e6bd4812997b206a3
유현 <cadry238@gmail.com>
2026-03-23 20:34:48 +0900

    [#2] 백준 15650

---
+++ baekjoon/15650.py
@@ -0,0 +1,17 @@
+import sys
+input = sys.stdin.readline
+
+
+
+def backtrack(start, path):
+  if len(path) == M :
+    print(' '.join(map(str, path)))
+    return
+  
+  for i in range(start, N + 1):
+    path.append(i)
+    backtrack(i + 1, path)
+    path.pop()
+
+N, M = map(int, input().split())
+backtrack(1, [])
